7a9c5ca035a1d3ff7c42810031a426e93601ec28
skosarew <skosarew@mail.ru>
2020-10-14 15:57:39 +0300

    Add dynamic URLs

--- accounts/views.py
+++ accounts/views.py
@@ -26,5 +26,9 @@ def products(requst):
     return render(requst, 'accounts/products.html', {'products': products})
 
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    orders_count = orders.count()
+    context = {'customer': customer, 'orders': orders, 'orders_count': orders_count}
+    return render(request, 'accounts/customer.html', context)
