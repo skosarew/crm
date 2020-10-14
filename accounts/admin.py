0ac3e0f68cccd296ec1e1c424b6beaf5112bf591
skosarew <skosarew@mail.ru>
2020-10-14 13:02:28 +0300

    Create relationships

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Customer, Order, Product
+from .models import Customer, Order, Product, Tag
 
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(Product)
+admin.site.register(Tag)
 admin.site.register(Order)
